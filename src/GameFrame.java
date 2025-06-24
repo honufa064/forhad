7e31020ac86dcacfd712b2d81ecd3e521e0d5056
FORHAD <forhadulislam@gmail.com>
2025-06-24 11:42:07 +0900

    11-1...11-2

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -5,14 +5,27 @@ public class GameFrame extends MyFrame {
 		GameWorld.Player=new Player(100,300,0,0);
 		addKeyListener(GameWorld.Player);
 		GameWorld.playerBullets=new Vector<PlayerBullet>();
+		GameWorld.enemies=new Vector<Enemy>();
+		GameWorld.enemies.add(new EnemyBase(100,50,1,0));
 		 while(true) {
 			 clear();
 			 GameWorld.Player.draw(this);
 			 GameWorld.Player.move();
 			 movePlayerBullets();
+			 moveEnemies();
 			 sleep(0.03);
 		 }
 	}
+		public void moveEnemies() {	 
+			 for (int i=0 ; i<GameWorld.enemies.size();i++) {
+				 Enemy e=GameWorld.enemies.get(i);
+				 e.draw(this);
+				 e.move();
+			 }
+			
+			 
+		 }
+	
 		public void movePlayerBullets() {	
 			 int i=0;
 			 while (i<GameWorld.playerBullets.size()) {
--- src/GameWorld.java
+++ src/GameWorld.java
@@ -3,6 +3,7 @@ import java.util.Vector;
 public class GameWorld {
 	static Player Player;
 	static Vector<PlayerBullet> playerBullets;
+	static Vector<Enemy> enemies;
 	
 
 }
